0c8a36982331206a13289e763575dee7a4145efb
c.hosh <c.hosh@bisan.com>
2024-02-19 14:41:46 +0200

    notifications

--- src/main/java/com/nomad/socialspring/notification/NotificationResponse.java
+++ src/main/java/com/nomad/socialspring/notification/NotificationResponse.java
@@ -1,21 +1,27 @@
 package com.nomad.socialspring.notification;
 
-import com.nomad.socialspring.user.UserResponseR;
-import lombok.Builder;
+import com.nomad.socialspring.common.BaseResponse;
+import com.nomad.socialspring.user.UserResponse;
+import lombok.Getter;
 
-import java.sql.Timestamp;
+@Getter
+public class NotificationResponse extends BaseResponse {
+  
+  private final String content;
+  private final UserResponse author;
+  private final NotificationType type;
+  private final Long entityId;
 
-/**
- * Response DTO for {@link Notification}
- */
-@Builder
-public record NotificationResponse(
-        Long id,
-        Timestamp createdOn,
-        Timestamp updatedOn,
-        NotificationType notificationType,
-        String content,
-        Long entityId,
-        UserResponseR author
-) {
+  private NotificationResponse(Notification notification) {
+    super(notification);
+    
+    content = notification.getContent();
+    author = notification.getAuthor().toResponse();
+    type = notification.getNotificationType();
+    entityId = notification.getEntityId();
+  }
+  
+  public static NotificationResponse fromEntity(Notification notification) {
+    return notification == null ? null : new NotificationResponse(notification);
+  }
 }
