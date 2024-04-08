b7ae3fc22df2458daeec4c75d8306fb1a85a84c4
Christian Hosh <104357056+ChristianHosh@users.noreply.github.com>
2024-04-08 21:39:41 +0200

    - configured image caching

--- src/main/java/com/nomad/socialspring/SocialSpringApplication.java
+++ src/main/java/com/nomad/socialspring/SocialSpringApplication.java
@@ -2,10 +2,15 @@ package com.nomad.socialspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAsync
+@EnableCaching
 @EnableScheduling
 @SpringBootApplication
 public class SocialSpringApplication {
@@ -14,4 +19,8 @@ public class SocialSpringApplication {
     SpringApplication.run(SocialSpringApplication.class, args);
   }
 
+  @Bean
+  public CacheManager cacheManager() {
+    return new ConcurrentMapCacheManager("images");
+  }
 }
