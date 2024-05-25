a99e3d82f4f9c0becbec5da463c121b51e857093
Christian Hosh <104357056+ChristianHosh@users.noreply.github.com>
2024-05-25 18:00:45 +0300

    reformat shit

--- src/main/java/com/nomad/socialspring/review/ReviewFacade.java
+++ src/main/java/com/nomad/socialspring/review/ReviewFacade.java
@@ -14,16 +14,6 @@ public class ReviewFacade {
 
   private final ReviewRepository repository;
 
-
-  public Review save(ReviewRequest reviewRequest, User author, User recipient) {
-    return save(Review.builder()
-        .content(reviewRequest.content())
-        .rating(reviewRequest.rating())
-        .author(author)
-        .recipient(recipient)
-        .build());
-  }
-
   public Review save(Review review) {
     return repository.save(review);
   }
